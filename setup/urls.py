a018ac7bd48ab567bee338647df4c5873365edf7
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-03 13:55:08 -0300

    algumas variaveis renomeadas

--- setup/urls.py
+++ setup/urls.py
@@ -13,7 +13,7 @@ router.register('imagens', ImagemViewSet, basename='Imagens')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
-    path('', include(router.urls)),
-    path('relatorios/<int:rel_id>/imagens/', ListaImagensPorRelatorio.as_view()),
-    path('relatorios/<int:rel_id>/circuitos/', ListaCircuitosPorRelatorio.as_view()),
+    path('api/', include(router.urls)),
+    path('api/relatorios/<int:rel_id>/imagens/', ListaImagensPorRelatorio.as_view()),
+    path('api/relatorios/<int:rel_id>/circuitos/', ListaCircuitosPorRelatorio.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
