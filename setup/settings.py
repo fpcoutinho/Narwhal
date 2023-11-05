9ef9424f5e3d0d1fef78239d5ce7a927ad0f6e84
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-05 19:06:31 -0300

    update cors

--- setup/settings.py
+++ setup/settings.py
@@ -102,11 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 # Rest Framework settings
